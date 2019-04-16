d0da626be8fca11b2f7564c85cd2c0885b25382c
danielcavalcanteinf <daniel.almeida.cavalcante@gmail.com>
2019-04-15 21:53:44 -0300

    Iniciando "projeto"

---
+++ Calculadora de Churrasco/script.js
@@ -0,0 +1,7 @@
+function calcularCarnes() {
+    var form = document.getElementById('formulario');
+    /*var qtHomens = document.getElementById('Homem');
+    var qtMulheres = document.getElementById('Mulher');*/
+    alert('O mouse clicou aqui!');
+
+} 
